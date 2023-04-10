c36eff970a34af813f172c8993cb166846c93efd
Anthony Mack <mackattack713@gmail.com>
2023-04-10 10:29:26 -0500

    Began working on chapter 8, exercise 1

--- chapter_8/exercise1.py
+++ chapter_8/exercise1.py
@@ -5,3 +5,17 @@
 # write a function called middle that takes a list and returns a
 # new list that contains all but the first and last elements.
 
+
+# myList = inp("Please enter a list of entries: ")
+def chop():
+    entry = input("Please enter a list of entries: ")
+    myList = entry.split()
+    orig = myList[:]
+    del myList[0]
+    del myList[len(myList) - 1]
+    print("Here's the original list:")
+    print(orig)
+    print("Here's the updated list:")
+    print(myList)
+
+chop()
